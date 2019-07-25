c4a06dbca62f02279e97f75467d0d207ac0b51c3
Yonnys Quintero <leafar.qr@gmail.com>
2019-07-25 15:26:06 -0400

    add sigle post

--- wp-content/themes/project3d/page-blog.php
+++ wp-content/themes/project3d/page-blog.php
@@ -4,17 +4,19 @@
 Latest blog articles</h1>
 <div class="card-all container">
  <div class="row">
+  <?php $args = array( 'post_type' => 'post', 'posts_per_page' => 3); ?>   
+          <?php $loop = new WP_Query( $args ); ?>
+           <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
    <div class="col-md-6">
-    <div class="blog-card spring-fever" style=" background: url('https://www.prensa.com/fotogalerias/holograma-interactivo-sobreviviente-Adina-Sella_LPRIMA20171027_0118_35.jpg');      background-size: cover;
+    <div class="blog-card spring-fever" style=" background: url('<?php echo get_the_post_thumbnail_url(); ?>');      background-size: cover;
                 background-position: center;background-repeat: no-repeat;">
         <div class="title-content">
-            <h3><a href="#">Portable holographic projector</a></h3>
-            <div class="intro"> <a href="#">Categoria</a> </div>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <div class="intro"> <?php the_category(); ?> </div>
         </div>
         <div class="card-info">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim...
-            <a href="#" class="cta">
+            <?php the_content(); ?>
+            <a href="<?php the_permalink(); ?>" class="cta">
               <span>See More</span>
               <i class="fa fa-arrow-right" aria-hidden="true"></i>
             </a>    
@@ -23,25 +25,7 @@ Latest blog articles</h1>
         <div class="color-overlay"></div>
     </div><!-- /.blog-card -->
    </div>
-    <div class="col-md-6">
-      <div class="blog-card spring-fever" style=" background: url('https://img.newatlas.com/telehuman-holo-1.jpg?auto=format%2Ccompress&ch=Width%2CDPR&fit=crop&h=347&q=60&rect=0%2C0%2C1799%2C1012&w=616&s=db7efc68812e83dc5fe17b4582450dd9');      background-size: cover;
-                  background-position: center;background-repeat: no-repeat;">
-          <div class="title-content">
-              <h3><a href="#">Portable holographic projector</a></h3>
-              <div class="intro"> <a href="#">Categoria</a> </div>
-          </div>
-          <div class="card-info">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua. Ut enim ad minim...
-              <a href="#" class="cta">
-              <span>See More</span>
-              <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            </a>           </div>
-
-          <div class="gradient-overlay"></div>
-          <div class="color-overlay"></div>
-      </div><!-- /.blog-card -->
-   </div>
+   <?php endwhile; ?>
  </div>
   
   
--- wp-content/themes/project3d/single.php
+++ wp-content/themes/project3d/single.php
@@ -1,1 +1,11 @@
-single.php
+<?php get_header(); ?>
+<?php  get_template_part('partials/nav'); ?>
+<h1 class="title-product">
+<?php the_title(); ?></h1>
+<div class="container">
+  <img src="<?php echo get_the_post_thumbnail_url(); ?>" style="width: 100%;">
+
+  <div class="container" style="color: white;"><p><?php the_excerpt(); ?>  </p></div> 
+ 
+</div>
+<?php get_footer(); ?>
