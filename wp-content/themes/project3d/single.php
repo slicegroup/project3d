8a17d44cc3c7a4ca5fe8544fd939d0e74dac9cfb
patricia <yively.pa@gmail.com>
2019-07-26 15:13:30 -0400

    add blog styles

--- wp-content/themes/project3d/single.php
+++ wp-content/themes/project3d/single.php
@@ -1,11 +1,22 @@
 <?php get_header(); ?>
 <?php  get_template_part('partials/nav'); ?>
-<h1 class="title-product">
-<?php the_title(); ?></h1>
-<div class="container">
-  <img src="<?php echo get_the_post_thumbnail_url(); ?>" style="width: 100%;">
 
-  <div class="container" style="color: white;"><p><?php the_excerpt(); ?>  </p></div> 
- 
+ <div class="blog-content-detail container">
+  <div class="row">
+    <div class="col-md-8">
+      
+      <div class="">
+        <img src="<?php echo get_the_post_thumbnail_url(); ?>" style="width: 100%;">
+        <h1 class="title-product">
+      <?php the_title(); ?></h1>
+        <div class="" style="color: white;"><p><?php the_excerpt(); ?>  </p></div> 
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Featured Products</h3>
+   
+    </div>
+  </div>
 </div>
+<div class="gradient-dark" ></div>
 <?php get_footer(); ?>
